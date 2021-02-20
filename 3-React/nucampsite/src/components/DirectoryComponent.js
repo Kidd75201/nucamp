cb8ba9e2d7b1cc8c8e628ca532014372d7c23574
Kidd75201 <jbezal@gmail.com>
2021-02-19 23:36:19 -0600

    Redux Thunk

--- 3-React/nucampsite/src/components/DirectoryComponent.js
+++ 3-React/nucampsite/src/components/DirectoryComponent.js
@@ -8,6 +8,7 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 // Functional Component with props -- render each card with different campsite details
 function RenderDirectoryItem({ campsite }) {
@@ -27,13 +28,35 @@ function RenderDirectoryItem({ campsite }) {
 
 function Directory(props) {
   // pull data from each campsite using map() from array
-  const directory = props.campsites.map((campsite) => {
+  const directory = props.campsites.campsites.map((campsite) => {
     return (
       <div key={campsite.id} className="col-md-5 m-1">
         <RenderDirectoryItem campsite={campsite} />
       </div>
     );
   });
+
+  if (props.campsites.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+
+  if (props.campsites.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.campsites.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   // return for entire directory component
   return (
