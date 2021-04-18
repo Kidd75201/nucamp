86a2790d642c2906c895ca80d097044bbac0c6bf
Kidd75201 <jbezal@gmail.com>
2021-04-18 17:36:58 -0500

    Updated

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/index.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* POST home page. */
+router.post("/", function (req, res, next) {
+  res.end(
+    `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+  );
 });
 
 module.exports = router;
--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+/* POST home page. */
+router.post(function (req, res) {
+  res.end(
+    `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+  );
 });
 
 module.exports = router;
